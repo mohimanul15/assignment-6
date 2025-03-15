5e6129d37b2e768b511a4777ae3053937ce7cdbc
mohimanul15 <mohimanul15@gmail.com>
2025-03-15 07:04:48 +0600

    first day last conmmit

--- js/main.js
+++ js/main.js
@@ -14,11 +14,11 @@ function catSelector(category,id){
     const catButton = document.getElementsByClassName('single_cat');
 
     for(const element of catButton){
-        element.classList.remove('bg-btn_color_main', 'bg-opacity-20','rounded-full');
+        element.classList.remove('bg-btn_color_main', 'bg-opacity-20','rounded-full','border-opacity-100');
 
-        element.classList.add('rounded-2xl');
+        element.classList.add('rounded-2xl','border-opacity-20');
     }
 
-    document.getElementById(category+'_'+id).classList.add('bg-btn_color_main','bg-opacity-20','rounded-full');
+    document.getElementById(category+'_'+id).classList.add('bg-btn_color_main','bg-opacity-20','rounded-full','border-opacity-100');
 
 }
